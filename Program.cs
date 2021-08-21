0d6c30b5ab7a694e9feeb46114a6e625ac2bf0e4
PRAJAKTA <PRAJAKTA@LAPTOP-DQH2B71H>
2021-08-21 14:32:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+            static void Main()
+            {
+            //Any number devide by zero
+                int a = 20;
+                int b = 0;
+                int c;
+            Console.WriteLine("A VALUE = " + a);
+                Console.WriteLine("B VALUE = " + b);
+                  c = a / b;
+                Console.WriteLine("Error in Program , any number can not be divisible by 0" );
+                Console.ReadLine();
+
+            // int[ ]arr = {1}; arr[1]=100;
+
+         
+                // Declare an array of max index 4
+                int[] arr = { 1 };
+
+                // Display values of array elements
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+
+                try
+                {
+                    Console.WriteLine(arr[7]);
+                }
+                catch (IndexOutOfRangeException Length)
+                {
+                    Console.WriteLine("An Exception has occurred : {0}", Length.Message);
+                }
+            }
+
+        }
+        
+    }
+
+
